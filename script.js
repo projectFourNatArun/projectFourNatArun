558ec7cbe65ca25c53bb918a1999b31beedbf37d
Natalia <nathy.andreola@gmail.com>
2019-08-31 00:45:11 -0400

    Code to display individual day

--- script.js
+++ script.js
@@ -45,7 +45,8 @@ weatherApp.getWeather = function (location) {
         }
     }).then(function (result) {
         console.log("results", result);
-        weatherApp.displayWeather(result.forecast.forecastday, result.location);
+        weatherApp.forecastData = result.forecast.forecastday;
+        weatherApp.displayWeather(weatherApp.forecastData, result.location);
     })
 }
 
@@ -53,25 +54,51 @@ weatherApp.getWeather = function (location) {
 weatherApp.displayWeather = function (forecast, location) {
     $('.searchBlock').addClass('smallSearch');
     $('#jsCityName').html(`${location.name}, ${location.region}`);
-    forecast.forEach(function (day) {
-        weatherApp.weatherDayView(day);
+    $('#jsWeekContainer').empty();
+    forecast.forEach(function (day, index) {
+        weatherApp.weatherDayView(day, index);
     });
 }
 
-// Display weather of the day 
-weatherApp.weatherDayView = function (item) {
+// Display weather of the day for the week
+weatherApp.weatherDayView = function (item, index) {
     $('#jsWeekContainer').append(`
-        <div>
+        <div class="dayContainer" data-index="${index}">
             <p>${item.date}</p>
             <img src="${item.day.condition.icon}" alt="${item.day.condition.text}">
             <p>${item.day.mintemp_c}C</p>
             <p>${item.day.maxtemp_c}C</p>
         </div>  
     `);
-    // view day summary
 }
 
+$('#jsWeekContainer').on('click', '.dayContainer', function() {
+    // Find the index of the day 
+    const dayIndex = $(this).data('index');
+    // weather info for the array clicked
+    const data = weatherApp.forecastData[dayIndex];
 
+    $('#jsDetailedView').empty();
+    $('#jsDetailedView').append(`
+        <div>
+            <p>Avg. Temp ${data.day.avgtemp_c}</p>
+            <p>Min Temp ${data.day.mintemp_c}</p>
+            <p>Max Temp ${data.day.maxtemp_c}</p>
+        </div>
+        <div>
+            <p>${data.date}</p>
+            <img src="${data.day.condition.icon}" alt="${data.day.condition.text}">
+            <p>${data.day.condition.text}</p>
+            <p>Sunrise: ${data.astro.sunrise}</p>
+            <p>Sunset: ${data.astro.sunset}</p>
+        </div>
+        <div>
+            <p>Humidity: ${data.day.avghumidity}%</p>
+            <p>Wind: ${data.day.maxwind_kph} km/h</p>
+            <p>UV: ${data.day.uv} km/h</p>
+        </div> 
+    `);    
+});
 
 weatherApp.init = function () {
     weatherApp.formComplete();
